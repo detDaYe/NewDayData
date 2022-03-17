2e3073c992a3c74d5f1814e5d490676132029507
Romeo RKPK <mindaye8504@gmail.com>
2022-03-18 00:04:01 +0900

    init

---
+++ src/main/java/org/lumenk/newday/newdaydata/NewDayData.java
@@ -0,0 +1,19 @@
+package org.lumenk.newday.newdaydata;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NewDayData extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        getLogger().info("몰?루");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
